9185344bff422d3c7864d1ff491a308311f10a0d
eadrianc <102111847+eadrianc@users.noreply.github.com>
2022-08-09 04:33:37 -0300

    commit de incio

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+import Pokedex from "./components/Pokedex.js";
+
+function App() {
+  return (
+
+    <div>
+   <Pokedex />
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/components/Pokedex.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+export default class Pokedex extends React.Component {
+    constructor() {
+        super();
+    
+        this.state = { user: {} };
+    }
+    
+    componentDidMount() {
+        fetch("https://pokeapi.co/api/v2/pokemon" )
+        .then(response => response.json())
+        .then(user => this.setState({user: user}))
+    }
+    
+    render() {
+        const user = this.state.user;
+    
+        return <h1>hello, {user.id}</h1>;
+    }
+    
+    }
